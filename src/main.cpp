e214dcd99c52ff49cd547f1d3c67eeb9526ce21c
Deadeye5589 <deadeye5589@hotmail.com>
2023-05-25 11:30:37 +0200

    Spiffs Upload Page loading

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,11 @@
 #include <WiFi.h>
 #include <ESPAsyncWebServer.h>
 #include <AsyncTCP.h>
-#include "SPIFFS.h"
+#include <SPIFFS.h>
 
 // Create AsyncWebServer object on port 80
-AsyncWebServer server(80);
+AsyncWebServer *server;
+
 
 // Search for parameter in HTTP POST request
 const char* PARAM_INPUT_1 = "ssid";
@@ -32,11 +33,8 @@ IPAddress subnet(255, 255, 255, 0);
 unsigned long previousMillis = 0;
 const long interval = 10000;  // interval to wait for Wi-Fi connection (milliseconds)
 
-// Set LED GPIO
-const int ledPin = 38;
-// Stores LED state
-
-String ledState;
+// List of files
+String listFiles(bool ishtml = false);
 
 // Initialize SPIFFS
 void initSPIFFS() {
@@ -107,19 +105,171 @@ bool initWiFi() {
   return true;
 }
 
-// Replaces placeholder with LED state value
-String processor(const String& var) {
-  if(var == "STATE") {
-    if(digitalRead(ledPin)) {
-      ledState = "ON";
-    }
-    else {
-      ledState = "OFF";
-    }
-    return ledState;
-  }
-  return String();
+
+
+
+
+
+
+// source: https://github.com/CelliesProjects/minimalUploadAuthESP32
+String humanReadableSize(const size_t bytes) {
+  if (bytes < 1024) return String(bytes) + " B";
+  else if (bytes < (1024 * 1024)) return String(bytes / 1024.0) + " KB";
+  else if (bytes < (1024 * 1024 * 1024)) return String(bytes / 1024.0 / 1024.0) + " MB";
+  else return String(bytes / 1024.0 / 1024.0 / 1024.0) + " GB";
 }
+
+
+void notFound(AsyncWebServerRequest *request) {
+  String logmessage = "Client:" + request->client()->remoteIP().toString() + " " + request->url();
+  Serial.println(logmessage);
+  request->send(404, "text/plain", "URL Not found");
+}
+
+
+// handles uploads to the filserver
+void handleUpload(AsyncWebServerRequest *request, String filename, size_t index, uint8_t *data, size_t len, bool final) {
+  // make sure authenticated before allowing upload
+    String logmessage = "Client:" + request->client()->remoteIP().toString() + " " + request->url();
+    Serial.println(logmessage);
+
+    if (!index) {
+      logmessage = "Upload Start: " + String(filename);
+      // open the file on first call and store the file handle in the request object
+      request->_tempFile = SPIFFS.open("/" + filename, "w");
+      Serial.println(logmessage);
+    }
+
+    if (len) {
+      // stream the incoming chunk to the opened file
+      request->_tempFile.write(data, len);
+      logmessage = "Writing file: " + String(filename) + " index=" + String(index) + " len=" + String(len);
+      Serial.println(logmessage);
+    }
+
+    if (final) {
+      logmessage = "Upload Complete: " + String(filename) + ",size: " + String(index + len);
+      // close the file handle as the upload is now done
+      request->_tempFile.close();
+      Serial.println(logmessage);
+      request->redirect("/");
+    }
+}
+
+
+// list all of the files, if ishtml=true, return html rather than simple text
+String listFiles(bool ishtml) {
+  String returnText = "";
+  Serial.println("Listing files stored on SPIFFS");
+  File root = SPIFFS.open("/");
+  File foundfile = root.openNextFile();
+  if (ishtml) {
+    returnText += "<table><tr><th align='left'>Name</th><th align='left'>Size</th><th></th><th></th></tr>";
+  }
+  while (foundfile) {
+    if (ishtml) {
+      returnText += "<tr align='left'><td>" + String(foundfile.name()) + "</td><td>" + humanReadableSize(foundfile.size()) + "</td>";
+      returnText += "<td><button onclick=\"downloadDeleteButton(\'" + String(foundfile.name()) + "\', \'download\')\">Download</button>";
+      returnText += "<td><button onclick=\"downloadDeleteButton(\'" + String(foundfile.name()) + "\', \'delete\')\">Delete</button></tr>";
+    } else {
+      returnText += "File: " + String(foundfile.name()) + " Size: " + humanReadableSize(foundfile.size()) + "\n";
+    }
+    foundfile = root.openNextFile();
+  }
+  if (ishtml) {
+    returnText += "</table>";
+  }
+  root.close();
+  foundfile.close();
+  Serial.println("Listing completed");
+  return returnText;
+}
+
+
+
+// parses and processes webpages
+// if the webpage has %SOMETHING% or %SOMETHINGELSE% it will replace those strings with the ones defined
+String processor(const String& var) {
+  if (var == "FREESPIFFS") {
+    return humanReadableSize((SPIFFS.totalBytes() - SPIFFS.usedBytes()));
+  }
+
+  if (var == "USEDSPIFFS") {
+    return humanReadableSize(SPIFFS.usedBytes());
+  }
+
+  if (var == "TOTALSPIFFS") {
+    return humanReadableSize(SPIFFS.totalBytes());
+  }
+  else 
+    return 0;
+}
+
+void configureWebServer() {
+  // configure web server
+
+  // if url isn't found
+  server->onNotFound(notFound);
+
+  // run handleUpload function when any file is uploaded
+  server->onFileUpload(handleUpload);
+
+  // Route for root / web page
+  server->on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(SPIFFS, "/index.html", "text/html", false, processor);
+    });
+    server->serveStatic("/", SPIFFS, "/");
+
+  server->on("/file", HTTP_GET, [](AsyncWebServerRequest * request) {
+    String logmessage = "Client:" + request->client()->remoteIP().toString() + " " + request->url();
+      Serial.println(logmessage);
+
+      if (request->hasParam("name") && request->hasParam("action")) {
+        const char *fileName = request->getParam("name")->value().c_str();
+        const char *fileAction = request->getParam("action")->value().c_str();
+
+        logmessage = "Client:" + request->client()->remoteIP().toString() + " " + request->url() + "?name=" + String(fileName) + "&action=" + String(fileAction);
+
+        if (!SPIFFS.exists(fileName)) {
+          Serial.println(logmessage + " ERROR: file does not exist");
+          request->send(400, "text/plain", "ERROR: file does not exist");
+        } else {
+          Serial.println(logmessage + " file exists");
+          if (strcmp(fileAction, "download") == 0) {
+            logmessage += " downloaded";
+            request->send(SPIFFS, fileName, "application/octet-stream");
+          } else if (strcmp(fileAction, "delete") == 0) {
+            logmessage += " deleted";
+            SPIFFS.remove(fileName);
+            request->send(200, "text/plain", "Deleted File: " + String(fileName));
+          } else {
+            logmessage += " ERROR: invalid action param supplied";
+            request->send(400, "text/plain", "ERROR: invalid action param supplied");
+          }
+          Serial.println(logmessage);
+        }
+      } else {
+        request->send(400, "text/plain", "ERROR: name and action params required");
+      }
+  });
+
+  server->on("/listfiles", HTTP_GET, [](AsyncWebServerRequest * request)
+  {
+    String logmessage = "Client:" + request->client()->remoteIP().toString() + " " + request->url();
+      Serial.println(logmessage);
+      request->send(200, "text/plain", listFiles(true));
+ });
+}
+
+
+
+
+
+
+
+
+
+
 
 void setup() {
   // Serial port for debugging purposes
@@ -127,10 +277,6 @@ void setup() {
   Serial.begin(115200);
 
   initSPIFFS();
-
-  // Set GPIO 2 as an OUTPUT
-  pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, LOW);
   
   // Load values saved in SPIFFS
   ssid = readFile(SPIFFS, ssidPath);
@@ -138,25 +284,12 @@ void setup() {
   Serial.println(ssid);
   Serial.println(pass);
 
-  if(initWiFi()) {
-    // Route for root / web page
-    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
-      request->send(SPIFFS, "/index.html", "text/html", false, processor);
-    });
-    server.serveStatic("/", SPIFFS, "/");
-    
-    // Route to set GPIO state to HIGH
-    server.on("/on", HTTP_GET, [](AsyncWebServerRequest *request) {
-      digitalWrite(ledPin, HIGH);
-      request->send(SPIFFS, "/index.html", "text/html", false, processor);
-    });
+  server = new AsyncWebServer(80);
 
-    // Route to set GPIO state to LOW
-    server.on("/off", HTTP_GET, [](AsyncWebServerRequest *request) {
-      digitalWrite(ledPin, LOW);
-      request->send(SPIFFS, "/index.html", "text/html", false, processor);
-    });
-    server.begin();
+
+  if(initWiFi()) {
+    configureWebServer();
+    server->begin();
   }
   else {
     // Connect to Wi-Fi network with SSID and password
@@ -171,13 +304,13 @@ void setup() {
     Serial.println(IP); 
 
     // Web Server Root URL
-    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    server->on("/", HTTP_GET, [](AsyncWebServerRequest *request){
       request->send(SPIFFS, "/wifimanager.html", "text/html");
     });
     
-    server.serveStatic("/", SPIFFS, "/");
+    server->serveStatic("/", SPIFFS, "/");
     
-    server.on("/", HTTP_POST, [](AsyncWebServerRequest *request) {
+    server->on("/", HTTP_POST, [](AsyncWebServerRequest *request) {
       int params = request->params();
       for(int i=0;i<params;i++){
         AsyncWebParameter* p = request->getParam(i);
@@ -205,7 +338,7 @@ void setup() {
       delay(3000);
       ESP.restart();
     });
-    server.begin();
+    server->begin();
   }
 }
 

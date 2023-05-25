c432312326c3ac75dd01db638915cdf4978e3850
Deadeye5589 <deadeye5589@hotmail.com>
2023-05-25 23:58:13 +0200

    Write to SD card and Encoder prototype

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,9 @@
 #include <ESPAsyncWebServer.h>
 #include <AsyncTCP.h>
 #include <SPIFFS.h>
+#include <SPI.h>
+#include <SD.h>
+#include <AiEsp32RotaryEncoder.h>
 
 // Create AsyncWebServer object on port 80
 AsyncWebServer *server;
@@ -17,7 +20,7 @@ const char* PARAM_INPUT_2 = "pass";
 String ssid;
 String pass;
 
-// File paths to save input values permanently
+//File paths to save input values permanently
 const char* ssidPath = "/ssid.txt";
 const char* passPath = "/pass.txt";
 
@@ -28,6 +31,23 @@ const char* localpwd = "hardies42";
 IPAddress localIP(4, 3, 2, 1); // hardcoded
 IPAddress localGateway(4, 3, 2, 1); //hardcoded
 IPAddress subnet(255, 255, 255, 0);
+
+
+//Defines SPI pins used for SD Card interface
+#define SD_CS_PIN        10    
+#define SD_CLK_PIN       12
+#define SD_MOSI_PIN      13
+#define SD_MISO_PIN      11
+
+
+/*Define Volume Encoder Pins and Classes*/
+#define ROTARY_ENCODER_A_PIN 44
+#define ROTARY_ENCODER_B_PIN 18
+#define ROTARY_ENCODER_BUTTON_PIN 43
+#define ROTARY_ENCODER_VCC_PIN -1
+#define ROTARY_ENCODER_STEPS 2
+
+AiEsp32RotaryEncoder rotaryEncoder = AiEsp32RotaryEncoder(ROTARY_ENCODER_A_PIN, ROTARY_ENCODER_B_PIN, ROTARY_ENCODER_BUTTON_PIN, ROTARY_ENCODER_VCC_PIN, ROTARY_ENCODER_STEPS);
 
 // Timer variables
 unsigned long previousMillis = 0;
@@ -106,9 +126,50 @@ bool initWiFi() {
 }
 
 
+void initSDCARD() {
+  pinMode(SD_CS_PIN, OUTPUT); // SS
+
+  SPI.begin(SD_CLK_PIN, SD_MOSI_PIN, SD_MISO_PIN, -1);
+  if (!SD.begin(SD_CS_PIN))
+  {
+    Serial.println("Card Mount Failed");
+    return;
+  }
+  
+  uint8_t cardType = SD.cardType();
+  if (cardType == CARD_NONE)
+  {
+    Serial.println("No SD card attached");
+    return;
+  }
+
+  Serial.print("SD Card Type: ");
+
+  if (cardType == CARD_MMC)
+  {
+    Serial.println("MMC");
+  }
+  else if (cardType == CARD_SD)
+  {
+    Serial.println("SDSC");
+  }
+  else if (cardType == CARD_SDHC)
+  {
+    Serial.println("SDHC");
+  }
+  else
+  {
+    Serial.println("UNKNOWN");
+  }
+
+  uint64_t cardSize = SD.cardSize() / (1024 * 1024);
+  Serial.printf("SD Card Size: %lluMB\n", cardSize);
+}
 
 
+void initEncoder() {
 
+}
 
 
 // source: https://github.com/CelliesProjects/minimalUploadAuthESP32
@@ -136,7 +197,7 @@ void handleUpload(AsyncWebServerRequest *request, String filename, size_t index,
     if (!index) {
       logmessage = "Upload Start: " + String(filename);
       // open the file on first call and store the file handle in the request object
-      request->_tempFile = SPIFFS.open("/" + filename, "w");
+      request->_tempFile = SD.open(String("/") + filename, FILE_WRITE);
       Serial.println(logmessage);
     }
 
@@ -160,8 +221,8 @@ void handleUpload(AsyncWebServerRequest *request, String filename, size_t index,
 // list all of the files, if ishtml=true, return html rather than simple text
 String listFiles(bool ishtml) {
   String returnText = "";
-  Serial.println("Listing files stored on SPIFFS");
-  File root = SPIFFS.open("/");
+  Serial.println("Listing files stored on SDCard");
+  File root = SD.open("/");
   File foundfile = root.openNextFile();
   if (ishtml) {
     returnText += "<table><tr><th align='left'>Name</th><th align='left'>Size</th><th></th><th></th></tr>";
@@ -230,17 +291,17 @@ void configureWebServer() {
 
         logmessage = "Client:" + request->client()->remoteIP().toString() + " " + request->url() + "?name=" + String(fileName) + "&action=" + String(fileAction);
 
-        if (!SPIFFS.exists(String("/")+fileName)) {
+        if (!SD.exists(String("/")+fileName)) {
           Serial.println(logmessage + " ERROR: file does not exist");
           request->send(400, "text/plain", "ERROR: file does not exist");
         } else {
           Serial.println(logmessage + " file exists");
           if (strcmp(fileAction, "download") == 0) {
             logmessage += " downloaded";
-            request->send(SPIFFS, String("/")+fileName, "application/octet-stream");
+            request->send(SD, String("/")+fileName, "application/octet-stream");
           } else if (strcmp(fileAction, "delete") == 0) {
             logmessage += " deleted";
-            SPIFFS.remove(String("/")+fileName);
+            SD.remove(String("/")+fileName);
             request->send(200, "text/plain", "Deleted File: " + String(fileName));
           } else {
             logmessage += " ERROR: invalid action param supplied";
@@ -266,9 +327,9 @@ void configureWebServer() {
 
 
 
-
-
-
+void IRAM_ATTR readEncoderISR(){
+  rotaryEncoder.readEncoder_ISR();
+}
 
 
 void setup() {
@@ -277,6 +338,16 @@ void setup() {
   Serial.begin(115200);
 
   initSPIFFS();
+
+  initSDCARD();
+
+
+  pinMode(ROTARY_ENCODER_A_PIN, INPUT_PULLUP);
+  pinMode(ROTARY_ENCODER_B_PIN, INPUT_PULLUP);
+  rotaryEncoder.begin();
+  rotaryEncoder.setup(readEncoderISR);
+  rotaryEncoder.setBoundaries(0, 100, 0);
+  rotaryEncoder.setAcceleration(10);
   
   // Load values saved in SPIFFS
   ssid = readFile(SPIFFS, ssidPath);
@@ -343,5 +414,18 @@ void setup() {
 }
 
 void loop() {
+  int8_t temp;
+  static unsigned long lastTimePressed = 0;
 
+   if (rotaryEncoder.encoderChanged()) {
+    Serial.print("Encoder Value: ");
+    Serial.println(rotaryEncoder.readEncoder());
+   }
+   if (rotaryEncoder.isEncoderButtonClicked()) {
+    
+    if (millis() - lastTimePressed < 500)
+        return; //ignore multiple press in that time milliseconds
+    lastTimePressed = millis();
+    Serial.println("Click");
+   }
 }

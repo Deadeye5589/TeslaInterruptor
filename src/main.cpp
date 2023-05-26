2d34e63f62d1ee14aa7bcd3e3cf5cb991eae66cd
Deadeye5589 <deadeye5589@hotmail.com>
2023-05-26 14:23:14 +0200

    Display Prototype added

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,9 @@
 #include <SPIFFS.h>
 #include <SPI.h>
 #include <SD.h>
-#include <AiEsp32RotaryEncoder.h>
+#include <lvgl.h>
+#include <TFT_eSPI.h>
+#include "ui.h"
 #include "encoder.h"
 
 // Create AsyncWebServer object on port 80
@@ -39,6 +41,16 @@ IPAddress subnet(255, 255, 255, 0);
 #define SD_CLK_PIN       12
 #define SD_MOSI_PIN      13
 #define SD_MISO_PIN      11
+
+
+/*Change to your screen resolution*/
+static const uint16_t screenWidth  = 320;
+static const uint16_t screenHeight = 170;
+
+static lv_disp_draw_buf_t draw_buf;
+static lv_color_t buf[ screenWidth * screenHeight / 10 ];
+
+TFT_eSPI tft = TFT_eSPI(); /* TFT instance */
 
 
 // Timer variables
@@ -313,6 +325,47 @@ void configureWebServer() {
 
 
 
+/* Display flushing */
+void my_disp_flush( lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p )
+{
+    uint32_t w = ( area->x2 - area->x1 + 1 );
+    uint32_t h = ( area->y2 - area->y1 + 1 );
+
+    tft.startWrite();
+    tft.setAddrWindow( area->x1, area->y1, w, h );
+    tft.pushColors( ( uint16_t * )&color_p->full, w * h, true );
+    tft.endWrite();
+
+    lv_disp_flush_ready( disp );
+}
+
+/*Read the touchpad*/
+void my_touchpad_read( lv_indev_drv_t * indev_driver, lv_indev_data_t * data )
+{
+    uint16_t touchX = 0, touchY = 0;
+
+    bool touched = false;//tft.getTouch( &touchX, &touchY, 600 );
+
+    if( !touched )
+    {
+        data->state = LV_INDEV_STATE_REL;
+    }
+    else
+    {
+        data->state = LV_INDEV_STATE_PR;
+
+        /*Set the coordinates*/
+        data->point.x = touchX;
+        data->point.y = touchY;
+
+        Serial.print( "Data x " );
+        Serial.println( touchX );
+
+        Serial.print( "Data y " );
+        Serial.println( touchY );
+    }
+}
+
 
 
 void setup() {
@@ -326,6 +379,32 @@ void setup() {
 
   initEncoder();
 
+  lv_init();
+
+  tft.begin();          /* TFT init */
+  tft.setRotation( 3 ); /* Landscape orientation, flipped */
+
+  lv_disp_draw_buf_init( &draw_buf, buf, NULL, screenWidth * screenHeight / 10 );
+
+  /*Initialize the display*/
+  static lv_disp_drv_t disp_drv;
+  lv_disp_drv_init( &disp_drv );
+  /*Change the following line to your display resolution*/
+  disp_drv.hor_res = screenWidth;
+  disp_drv.ver_res = screenHeight;
+  disp_drv.flush_cb = my_disp_flush;
+  disp_drv.draw_buf = &draw_buf;
+  lv_disp_drv_register( &disp_drv );
+
+  /*Initialize the (dummy) input device driver*/
+    static lv_indev_drv_t indev_drv;
+    lv_indev_drv_init( &indev_drv );
+    indev_drv.type = LV_INDEV_TYPE_POINTER;
+    indev_drv.read_cb = my_touchpad_read;
+    lv_indev_drv_register( &indev_drv );
+
+
+  ui_init();
 
  
   // Load values saved in SPIFFS
@@ -397,8 +476,11 @@ void loop() {
     Serial.print("Encoder Value: ");
     Serial.println(rotaryencodervalue());
    }
-   
+
    if (rotaryencoderbuttonpressed()) {
     Serial.println("Click");
    }
+
+   lv_timer_handler(); /* let the GUI do its work */
+   delay(5);
 }
